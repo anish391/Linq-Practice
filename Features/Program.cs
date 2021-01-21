dee2ee71580f0748e70c274a03f1ce42414c5d2a
Anish Gadekar <argads@gmail.com>
2021-01-21 12:01:20 -0500

    Features - Refactor

--- Features/Program.cs
+++ Features/Program.cs
@@ -16,13 +16,13 @@ namespace Features
 				new Employee { Id = 2, Name= "Chrissy" },
 				new Employee { Id = 4, Name= "Adam" }
 			};
-			// Named method approach
 			IEnumerable<Employee> sales = new List<Employee>()
 			{
 				new Employee { Id = 3, Name = "Alex" }
 			};
-			foreach (var employee in developers.Where(e => e.Name.Length == 5)
-											   .OrderBy(e => e.Name))
+
+			var query = developers.Where(e => e.Name.Length == 5).OrderBy(e => e.Name);
+			foreach (var employee in query)
 			{
 				Console.WriteLine(employee.Name);
 			}
