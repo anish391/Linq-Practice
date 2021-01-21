1bf35542cd95cfbad06f12023fca7a16401c342a
Anish Gadekar <argads@gmail.com>
2021-01-21 11:39:51 -0500

    Features - Add Named Method and Lambda Expression

--- Features/Program.cs
+++ Features/Program.cs
@@ -6,26 +6,34 @@ using System.Threading.Tasks;
 
 namespace Features
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            IEnumerable<Employee> developers = new Employee[]
-            {
-                new Employee { Id = 1, Name= "Scott" },
-                new Employee { Id = 2, Name= "Chris" }
-            };
-
-            IEnumerable<Employee> sales = new List<Employee>()
-            {
-                new Employee { Id = 3, Name = "Alex" }
-            };
-            Console.WriteLine(developers.Count());
-            IEnumerator<Employee> enumerator = developers.GetEnumerator();
-            while (enumerator.MoveNext())
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			IEnumerable<Employee> developers = new Employee[]
 			{
-                Console.WriteLine(enumerator.Current.Name);
+				new Employee { Id = 1, Name= "Scott" },
+				new Employee { Id = 2, Name= "Chris" }
+			};
+			// Named method approach
+			IEnumerable<Employee> sales = new List<Employee>()
+			{
+				new Employee { Id = 3, Name = "Alex" }
+			};
+			foreach (var employee in developers.Where(NameStartsWithS))
+			{
+				Console.WriteLine(employee.Name);
 			}
-        }
-    }
+			/* Lamba Expression Approach
+			foreach (var employee in developers.Where(e => e.Name.StartsWith("S")))
+			{
+				Console.WriteLine(employee.Name);
+			}*/
+		}
+
+		private static bool NameStartsWithS(Employee employee)
+		{
+			return employee.Name.StartsWith("S");
+		}
+	}
 }
